d02050bb2075e06321d03d5537d10370c6e84f5a
myrna <myrnapt96@gmail.com>
2026-02-23 19:00:20 +0100

    Comentarios sobre comandos

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,16 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+#git branch
+#git add .
+#git commit -m "Mensaje claro del cambio"
+#git push origin feature/ci-workflow
+#Ve al repo en GitHub.
+#Aparecerá botón amarillo:
+#Compare & pull request
+#Haz clic.
+#Asegúrate:
+#Base: main
+#Compare: feature/ci-workflow
+#Pulsa Create pull request.
