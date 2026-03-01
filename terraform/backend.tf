090e7e7663b267dfc78c93ce3b503c7439700f03
Joel Sánchez Simón <jsa5214@office.proven.cat>
2026-03-01 20:10:10 +0100

    style(terraform): auto-format backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "fila2-terraform-state-2026-project"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "fila2-terraform-state-2026-project"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "terraform-locks"
   }
 }
